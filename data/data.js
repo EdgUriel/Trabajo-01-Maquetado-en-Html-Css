61cf14480cedb0eb8e78b7bf67a0f0ec69faf176
Edgar Uriel Briseño Rodriguez <edgarurieludg@gmail.com>
2024-08-20 23:29:57 -0600

    Proyect added

---
+++ data/data.js
@@ -0,0 +1,168 @@
+
+export const introductionData = {
+    // Hi, my name is....
+    yourName: "Edgar Uriel Briseño Rodriguez", 
+    socialNetwork: [
+        {
+            icon: "fa fa-envelope-o",  
+            nameSocialNetwork: "Email",  
+            url: "#" 
+        },
+        {
+            icon: "fa fa-linkedin-square",
+            nameSocialNetwork: "Linkedin",
+            url: "https://www.linkedin.com/in/edgarurielbrodriguez/"
+        },
+        {
+            icon: "fa fa-github-alt",
+            nameSocialNetwork: "Github",
+            url: "https://github.com/EdgUriel"
+        }
+
+
+    ],
+
+    // Page session direction
+    nav: [
+        {
+            optionNav: "About me",
+            idSection: "about"    
+        },
+        {
+            optionNav: "Skills",
+            idSection: "skills"  
+        },
+        {
+            optionNav: "Projects",
+            idSection: "projects"  
+        }
+
+
+    ]
+
+}
+
+
+//=========================================
+//                ABOUT ME
+//=========================================
+export const aboutMeData = {
+    title: "About Me",
+    name: "Edgar Uriel Briseño Rodriguez",
+    birth: "January 01, 2004",
+    phone: "+52 3310203881",
+    email: "edgarurieludg@gmail.com",
+    nationality: "Mexican"
+}
+
+
+
+
+//=========================================
+//                 SKILLS 
+//=========================================
+export const skillsData = [
+
+
+    {
+        img: "assets/cpp-logo.png", 
+        nameSkill: "C/C++",      
+        percentage: 75,
+        color: '#E34F26'
+    },
+    {
+        img: "assets/java.svg",
+        nameSkill: "Java",     
+        percentage: 70,
+        color: '#E34F26'
+    },
+    {
+        img: "assets/js.svg",
+        nameSkill: "JavaScript",
+        percentage: 50,
+        color:  '#E34F26'
+    },
+    {
+        img: "assets/html-css.png",
+        nameSkill: "HTML/CSS",
+        percentage: 60,
+        color:  '#E34F26'
+    }
+
+]
+//=========================================
+//                 LANGUAGES
+//=========================================
+export const languagesData = [
+
+
+    {
+        img: "assets/méxico.jpg",  
+        nameSkill: "Spanish",      
+        percentage: 95,
+        color: '#E34F26'
+    },
+    {
+        img: "assets/usa.png",
+        nameSkill: "English",      
+        percentage: 50,
+        color: '#E34F26'
+    }
+
+]
+//=========================================
+//                PROJECTS
+//=========================================
+export const projectsData = [
+    {
+        title: "SKELETAL SIEGE",   
+        describe: "2D game developed in the Unity videogame engine for the Hypermedia subject. A ninja in a post-apocalyptic world full of zombies and strange creatures. ",
+        url: "https://github.com/EdgUriel/Unity2D-Skeletal-Siege",  
+        img: "assets/skeletal-siege.png"  
+    },
+    {
+        title: "C-COCINÓ",
+        describe: "A social networking website to search and publish your favourite food recipes.",
+        url: "https://github.com/AlejjandroCovarrubias/ProyectoThelma",
+        img: "assets/c-cocinó.png"
+    },
+    {
+        title: "E-StockManager",
+        describe: "A website for inventory management with various integrated functionalities. ",
+        url: "https://github.com/EdgUriel/E_StockManager/tree/develop",
+        img: "assets/project1.jpg"
+    }
+]
+
+//=========================================
+//                 LANGUAGES
+//=========================================
+export const othersData = [
+
+
+    {
+        title: "Computer engineering student ",  
+        describe: "Centro Universitario de Cienca Exactas e Ingenierías (CUCEI) - Universidad de Guadalajara [2022 - present]"
+    },
+    {
+        title: "Data Science Course",
+        describe: "Gained expertise in data analysis, machine learning, and Python programming - Técnologico Nacional de México [2023]"
+    },
+    {
+        title: "Work experience ",  
+        describe: "No work experience at the moment. "
+    },
+    {
+        title: "Professional interests",
+        describe: "I am interested in learning in depth frameworks like Spring boot and later to do an internship in Oracle."
+    }
+
+]
+
+
+
+
+
+
+
+
